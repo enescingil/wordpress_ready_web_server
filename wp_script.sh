8eaafb445664917371a88577010dd72e45cc464d
enescingil <enescingil.od@gmail.com>
2020-03-06 05:23:28 +0000

    Added wordpress_setup function

--- wp_script.sh
+++ wp_script.sh
@@ -68,6 +68,78 @@ function mysql_setup(){
 	systemctl restart mysql
 }
 
+#Setup a Wordpress site
+function wordpress_setup(){
+
+	#Get username
+	read -p 'Please write down the username for your website: ' user_var
+	#Double check the username
+	echo $user_var
+	read -p 'Are you sure you wanted to use that username (y/n): ' user_sure
+	while [ "$user_sure" = "n" ];
+	do
+		read -p 'Please write down the username for your website: ' user_var
+		echo $user_var
+		read -p 'Are you sure you wanted to use that username (y/n): ' user_sure
+	done	
+	
+	#Create a system user for this site
+	#adduser $user_var
+	#mkdir -p /home/$user_var/logs
+
+	#Get IP Address or Host name
+	read -p 'Please write down your IP Address or domain name: ' user_ip
+	#Double check the IP Address
+	echo $user_ip
+	read -p 'Is this IP Address correct? (y/n): ' user_ip_sure
+	while [ "$user_ip_sure" = "n" ];
+	do
+		read -p 'Please write down your IP Address or domain name: ' user_ip
+		echo $user_ip
+		read -p 'Is this IP Address correct? (y/n): ' user_ip_sure
+	done
+
+	#Create nginx vhost config file
+	#touch $user_var.conf
+	#Change it's IP Address with the given one
+	sed -i 's/tutorial/'"$user_ip"'/gI' tutorial.conf
+	#Paste it in to the user's config file
+	mv tutorial.conf $user_var.conf
+	mv $user_var.conf /etc/nginx/conf.d/
+	#Disable default nginx vhost
+	rm /etc/nginx/sites-enabled/default
+	
+	#Get the site name
+	read -p 'Please write down your site name: ' user_site
+	#Double check the site name
+	echo $user_site
+	read -p 'Is this your sitename? (y/n): ' user_site_sure
+	while [ "$user_site_sure" = "n" ];
+	do
+		read -p 'Please write down your site name: ' user_site
+		echo $user_site
+		read -p 'Is this your sitename? (y/n): ' user_site_sure
+	done
+
+	#Create php-fpm vhost pool config file
+	sed -i 's/tutorial/'"$user_site"'/gI' tutorial2.conf
+	mv tutorial2.conf $user_var.conf
+	mv $user_var.conf /etc/php/7.2/fpm/pool.d/
+	#Create the php-fpm logfile
+	touch /home/$user_var/logs/phpfpm_error.log
+
+	#Create site database + DB User
+	#Get the mysql_password
+	
+
+	#if [ "$user_var" = "y" ]; then
+	#	echo "Thats it my boy"	
+	#else
+	#	read -p 'Please write down the username for your website: ' user_var
+	#fi
+
+}
+
 #Make sure only root can run it
 if [[ $EUID -ne 0 ]]; then
 	echo "This script must be run as a root" 1>&2
@@ -78,4 +150,5 @@ else
 	nginx_conf
 	php_extend
 	mysql_setup
+	wordpress_setup
 fi
